0c563e6f858597fe41dfd98e2b8d6fe70d50b775
Sean Cashin <scashin133@gmail.com>
2013-10-21 14:50:57 -0700

    remove task that doesnt exist

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,17 +28,6 @@ namespace :deploy do
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, 'cache:clear'
-        end
-      end
-    end
-  end
-
   after :finishing, 'deploy:cleanup'
 
 end
